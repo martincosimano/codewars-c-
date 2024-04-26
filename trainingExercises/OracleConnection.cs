20c973550b7a7c997f26bb47b61614ed661edb39
Martín Cosimano <martincosimanodev@gmail.com>
2024-04-26 17:21:16 -0300

    Add files via upload

---
+++ trainingExercises/OracleConnection.cs
@@ -0,0 +1,21 @@
+﻿namespace InheritancePractice
+{
+    public class OracleConnection : DbConnection
+    {
+        public override void OpenConnection()
+        {
+            Console.WriteLine("Connection with Oracle is open");
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("Connection with Oracle is closed");
+        }
+
+        public OracleConnection(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+    }
+}
