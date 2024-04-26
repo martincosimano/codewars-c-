58df45fac95a6b7c0af693f96b4cd5f3e98b6f5d
Martín Cosimano <martincosimanodev@gmail.com>
2024-04-26 17:23:06 -0300

    Add files via upload

---
+++ trainingExercises/polymorphism/SqlConnection.cs
@@ -0,0 +1,22 @@
+﻿namespace InheritancePractice
+{
+    public class SqlConnection : DbConnection
+    {
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine("Connection with SQL is open");
+        }
+        public override void CloseConnection()
+        {
+            Console.WriteLine("Connection with SQL is closed");
+        }
+
+        public SqlConnection(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+        
+    }
+}
