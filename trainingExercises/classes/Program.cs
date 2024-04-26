dbefc63685692f0609a3edd8c193469f598b848d
Martín Cosimano <martincosimanodev@gmail.com>
2024-04-26 17:33:02 -0300

    Add files via upload

---
+++ trainingExercises/classes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStopwatch = new Stopwatch();
+            myStopwatch.Start();
+            System.Threading.Thread.Sleep(2000);
+            myStopwatch.Stop();
+
+            Console.WriteLine(myStopwatch.Duration);
+        }
+    }
+}
---
+++ trainingExercises/classes/Stopwatch.cs
@@ -0,0 +1,40 @@
+﻿namespace Practice
+{
+    class Stopwatch
+    {
+        private DateTime? _startTime;
+        private TimeSpan _duration;
+        private bool _isRunning;
+
+        public TimeSpan Duration => _duration;
+
+        public void Start()
+        {
+            if (!_isRunning)
+            {
+                _startTime = DateTime.Now;
+                _isRunning = true;
+                Console.WriteLine("Start");
+            }
+            else
+            {
+                Console.WriteLine("Already running");
+            }
+
+        }
+        public void Stop()
+        {
+            if (_isRunning)
+            {
+                _duration += DateTime.Now - _startTime.Value;
+                _isRunning = false;
+                Console.WriteLine("Stop");
+            }
+            else
+            {
+                Console.WriteLine("Not running");
+            }
+
+        }
+    }
+}
